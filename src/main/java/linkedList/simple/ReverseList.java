87ab78079b9aeb03dcc7a28a287978a00f86e557
zhangyaqi02 <zhangyaqi02@58.com>
2025-10-16 17:41:42 +0800

    反转链表

--- src/main/java/linkedList/simple/ReverseList.java
+++ src/main/java/linkedList/simple/ReverseList.java
@@ -2,6 +2,18 @@ package linkedList.simple;
 
 public class ReverseList {
 
+    /**
+     * 通过临时变量，来处理链表反转问题
+     * 1. 通过临时变量，保存当前节点的下一个节点
+     * 2. 将当前节点的下一个节点指向新链表的头节点
+     * 3. 将新链表的头节点指向当前节点
+     * 4. 将当前节点指向临时变量
+     * 5. 重复以上步骤，直到当前节点为空
+     * 6. 返回新链表的头节点
+     *
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head){
 
         ListNode  newHead = new ListNode();
